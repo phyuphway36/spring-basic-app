b10f34ae1ffcdf22efa3563d5144b60b4ff485c5
PhyuPhway <phyuphyuphway1999@gmail.com>
2022-10-22 13:37:16 +0630

    Initial commit spring_basic_app

---
+++ src/main/java/com/hostmdy/jpa/SpringBasicsAppApplication.java
@@ -0,0 +1,13 @@
+package com.hostmdy.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hostmdy/jpa/SpringBasicsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostmdy.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
